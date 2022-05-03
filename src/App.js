e623737712c477025708abf681da9331cc20a51e
danielchwi <daniel.widodo@bitcoin.co.id>
2022-05-03 22:14:12 +0700

    setup page component and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
+import Home from './home/Home'
+import Create from './create/Create'
+import Search from './search/Search'
+import Recipe from './recipe/Recipe'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/"}>
+            <Home/>
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route exact path={"/create"}>
+            <Create/>
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route exact path={"/search"}>
+            <Search/>
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route exact path={"/recipe/:id"}>
+            <Recipe/>
+          </Route>
+        </Switch>
       
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/search/Search.js
@@ -0,0 +1,7 @@
+import './Search.js'
+
+export default function Search() {
+  return (
+    <div>Search</div>
+  )
+}
