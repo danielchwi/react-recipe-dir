da9ccdd18222dc4f127e8449168ebe9184fb087d
danielchwi <daniel.widodo@bitcoin.co.id>
2022-05-04 20:02:10 +0700

    add dark or light mode

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,21 +1,35 @@
 import { useTheme } from '../hooks/useTheme'
+import modeIcon from '../assets/mode-icon.svg'
 import './ThemeSelector.css'
 
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
 
-  const { changeColor } = useTheme()
+  const { changeColor, changeMode, mode } = useTheme()
+
+  const handleToggle = () => {
+      changeMode(mode === 'dark' ? "light" : "dark")
+      console.log(mode)
+  }
 
   return (
     <div className='theme-selector'>
+      <div className='mode-toggle'>
+        <img 
+            src={modeIcon}
+            onClick={handleToggle}
+            alt="dark/light mode toggle"
+            style={{filter: mode === "dark" ? 'invert(100%)' : 'invert(20%)'}}
+        />
+      </div>
       <div className='theme-buttons'>
         {themeColors.map(color => (
           <div
             key={color}
             style={{background: color}}
             onClick={() => changeColor(color)}
-          >
+          > 
           </div>
         ))}
       </div>
